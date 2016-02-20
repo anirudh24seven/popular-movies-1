d7609a959cc1234ad07e0dabbdfe5ad7020cfa6d
Anirudh S <anirudh24seven@gmail.com>
2016-02-20 16:27:57 +0530

    Refer to the API key in build.gradle from the gradle.properties file and read it in our app

--- app/src/main/java/com/anirudh24seven/popularmovies1/MainActivity.java
+++ app/src/main/java/com/anirudh24seven/popularmovies1/MainActivity.java
@@ -1,13 +1,18 @@
 package com.anirudh24seven.popularmovies1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String API_KEY = BuildConfig.TMDB_API_KEY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            
+
     }
 }
